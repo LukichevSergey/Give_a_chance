e67acdaa395de6c10f017c7e9a950e6f394715f9
Lukichev Sergey <Lukichev_s@mail.ru>
2022-06-21 06:05:49 +0700

    Initial Commit

---
+++ Give_a_chance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Give_a_chance
+//
+//  Created by Сергей Лукичев on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
